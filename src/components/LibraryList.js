cb1ee6b7c9cf54ec28ededc60fc3f2c02a9d278b
y0m0 <lixi.daniele@gmail.com>
2018-01-02 19:49:15 +0100

    Pass the Library data to the ListView

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.DataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow() {
+    return <ListItem />
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
