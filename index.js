7b722292bf1dbde486996887929d32ee20b2fe31
y0m0 <lixi.daniele@gmail.com>
2018-01-02 17:54:31 +0100

    Initialize App and add redux to the project

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('flash_cards', () => App);
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>Flash Card</View>
+  );
+};
+
+export default App;
